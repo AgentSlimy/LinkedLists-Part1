af90f28fe4deb8c207023f8fc7fb20534c6d5247
U-BSD\417197 <417197@SHS-10D9C06F2.beaverton.k12.or.us>
2022-01-03 12:14:57 -0800

    First

---
+++ student.h
@@ -0,0 +1,20 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+using namespace std;
+
+class Student {
+public:
+  Student();
+  ~Student();
+  char* getFirstName();
+  char* getLastName();
+  int* getID();
+  float* getGPA();
+private:
+  char firstName[20];
+  char lastName[20];
+  int ID;
+  float GPA;
+};
+#endif
