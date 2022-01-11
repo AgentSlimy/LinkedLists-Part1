abd8ecf823d6a2e96833d31d20d26a7b03872b41
U-BSD\417197 <417197@SHS-10D9C06F2.beaverton.k12.or.us>
2022-01-11 12:07:23 -0800

    halp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,58 @@
 #include <iostream>
 #include <cstring>
-#include "student.h"
+#include <iomanip>
+#include <stdio.h>
 #include "node.h"
+#include "student.h"
 
 using namespace std;
 
 int main() {
   char input[20];
-  
+
+  //First student
+  Student* student = new Student;
+  cout << "Student's first name: " << endl;
+  cin.get(student -> getFirstName(), 20);
+  cin.clear();
+  cin.ignore(10000, '\n');
+  cout << "Student's last name: " << endl;
+  cin.get(student -> getLastName(), 20);
+  cin.clear();
+  cin.ignore(10000, '\n');
+  cout << "Student's ID: " << endl;
+  cin >> *student -> getID();
+  cin.clear();
+  cin.ignore(10000, '\n');
+  cout << "Student's GPA: " << endl;
+  cin >> *student -> getGPA();
+  cin.clear();
+  cin.ignore(10000, '\n');
+
+  cout << "----------------------------" << endl;
+
+  //Second student
+  Student* student2 = new Student;
+  cout << "Student's first name: " << endl;
+  cin.get(student2 -> getFirstName(), 20);
+  cin.clear();
+  cin.ignore(10000, '\n');
+  cout << "Student's last name: " << endl;
+  cin.get(student2 -> getLastName(), 20);
+  cin.clear();
+  cin.ignore(10000, '\n');
+  cout << "Student's ID: " << endl;
+  cin >> *student2 -> getID();
+  cin.clear();
+  cin.ignore(10000, '\n');
+  cout << "Student's GPA: " << endl;
+  cin >> *student2 -> getGPA();
+  cin.clear();
+  cin.ignore(10000, '\n');
+
+  cout << "----------------------------" << endl;
 
   
+  
+  return 0;
 }
